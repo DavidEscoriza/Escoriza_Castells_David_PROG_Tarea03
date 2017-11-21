68cd3074068791692b794ecfcb51207ed78dbb70
David <David@Davi-PC>
2017-11-21 20:09:02 +0100

    reamos un constructor para la clase Rey que acepta como parámetros el color.

--- Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Rey.java
+++ Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Rey.java
@@ -7,4 +7,15 @@ package tarea03;
 public class Rey {
     private Color color;
     private Posicion posicion;
+    
+    public Rey(Color color){
+        this.color=color;
+        if (this.color==Color.BLANCO){
+            this.posicion.setFila(1);
+            this.posicion.setColumna('e');
+        } else {
+            this.posicion.setFila(8);
+            this.posicion.setColumna('e');
+        }
+    }
 }
