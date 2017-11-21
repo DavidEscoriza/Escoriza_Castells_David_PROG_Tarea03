6e353c9f0442c83ac7baf82c1308ae7ae611f05d
David <David@Davi-PC>
2017-11-21 19:58:35 +0100

    Añado comentarios tipo javadoc a la clase y a cada uno de los métodos.

--- Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Posicion.java
+++ Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Posicion.java
@@ -1,14 +1,19 @@
 package tarea03;
 
 /**
- *
+ * Generamos la clase Posicion con los atributos 'fila' y 'columna'.
  * @author David
  */
 public class Posicion {
     private int fila;
     private char columna;
-    
-    public Posicion (int fila, char columna){
+
+/**
+ * Constructor de clase.
+ * @param fila de tipo entero. Si error en la fila por defecto es 1.
+ * @param columna de tipo caracter. Si error en la columna por defecto es a.
+ */    
+    public Posicion (int fila, char columna){ 
         if (fila>=1 && fila<=8){
             this.fila=fila;
         } else {
@@ -21,14 +26,25 @@ public class Posicion {
         }
     }
     
+    /**
+     * Devuelve el valor de la posición fila.
+     * @return La int
+     */
     public int getFila(){
         return this.fila;
     }
-    
+    /**
+     * Devuelve el valor de la columna.
+     * @return char
+     */
     public char getColumna(){
         return this.columna;
     }
     
+    /**
+     * Da valor a la fila.
+     * @param fila int. Por defecto 1.
+     */
     public void setFila(int fila){
         if (fila>=1 && fila<=8){
             this.fila=fila;
@@ -37,6 +53,10 @@ public class Posicion {
         }
     }
     
+    /**
+     * Da valor a la columna.
+     * @param columna char. Por defecto a.
+     */
     public void setColumna(char columna){
         if (columna>='a' && columna<='h'){
             this.columna=columna;
@@ -45,6 +65,10 @@ public class Posicion {
         }        
     }
     
+    /**
+     * Devuelve una cadena con los valores de fila y columna.
+     * @return 
+     */
     public String toString(){
         return "Fila: "+this.fila+". Columna: "+this.columna;
     }
