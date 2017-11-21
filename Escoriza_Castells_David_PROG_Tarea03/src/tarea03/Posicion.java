c711f623f34b4f7e71b1330975b89d0ce4a43c51
David <David@Davi-PC>
2017-11-21 19:44:13 +0100

    Generamos el constructor Posicion con parametros fila (entero) y columna (caracter).

--- Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Posicion.java
+++ Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Posicion.java
@@ -7,4 +7,18 @@ package tarea03;
 public class Posicion {
     private int fila;
     private char columna;
+    
+    public Posicion (int fila, char columna){
+        if (fila>=1 && fila<=8){
+            this.fila=fila;
+        } else {
+            this.fila=1;
+        }
+        if (columna>='a' && columna<='h'){
+            this.columna=columna;
+        } else {
+            this.columna='a';
+        }
+    }
 }
+
