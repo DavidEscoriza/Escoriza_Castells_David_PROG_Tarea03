d89226b957802053e50bdbdfb1a2dd09ea6776dd
David <David@Davi-PC>
2017-11-21 19:35:57 +0100

    Generamos el enumerado Direccion.

---
+++ Escoriza_Castells_David_PROG_Tarea03/src/tarea03/Direccion.java
@@ -0,0 +1,9 @@
+package tarea03;
+
+/**
+ * Generamos el enumerado Direccion que puede tomar los valores NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE y NOROESTE
+ * @author David
+ */
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE
+}
